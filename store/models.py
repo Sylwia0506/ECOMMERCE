df732d1ec5b2f48c675541b04636136834c6ec6c
user <user@LAPTOP-C9FTB3AU>
2022-04-02 22:12:03 +0200

    add class product

--- store/models.py
+++ store/models.py
@@ -7,3 +7,32 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "categories"
+
+        def __str__(self):
+            return self.name
+
+
+class Product(models.Model):
+    category = model.ForeginKey(
+        Category, related_name="product", on_delete=models.CASCADE
+    )
+    created_by = models.ForeginKey(
+        User, on_delete=models.CASCADE, related_name="product_creator"
+    )
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default="admin")
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="images/")
+    slug = models.CharField(max_length=255, unique=True)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(add_now=True)
+    updated = models.DataTimeField(add_now=True)
+
+    class Meta:
+        verbose_name_plural = "Products"
+        ordering = ("-created",)
+
+    def __str__(self):
+        return self.title
