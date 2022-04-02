ba948482686ae8fd3334c9b69be914339b4e7fc7
user <user@LAPTOP-C9FTB3AU>
2022-04-02 21:52:09 +0200

    reformat file

--- store/models.py
+++ store/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    slug = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, db_index=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
